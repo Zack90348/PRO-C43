f9185b4e480801db41e8587e64ca8f02ec6f92b2
Sadhak <postsadhak@gmail.com>
2021-05-17 08:05:51 +0530

    Files added

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white");
+    textSize(15);
+    text("R E V B A I L A", 100,50);
+    fill("lightblue");
+    text("Hint: Always changing, not constant !!", 100,70);
+
+// add code for displaying rest of the names and the hints.
+    fill("white");
+    textSize(15);
+    text("A T A D A S B A ",700,160);
+    fill("lightblue");
+    text("Hint: Place where data is stored",700,180); 
+    
+    fill("white");
+    textSize(15);
+    text("C U T N I F O N ", 100,340);
+    fill("lightblue");
+    text("Hint: Performs a particular task", 100,360);    
+
+}
